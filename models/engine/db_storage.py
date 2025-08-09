f84a392a0c77aadd7ff2447aec283dc44dde27ac
HinnMachache <bennyhinn253.bh@gmail.com>
2025-08-09 02:06:24 +0100

    Added a switch transistion between FileStorage and DBStorage"

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,7 +1,13 @@
 """ DB Storage Module, Handles SQLAlchemy storage"""
 from models.base_model import Base
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 from sqlalchemy import create_engine
-from sqlalchemy.orm import create_session
+from sqlalchemy.orm import sessionmaker, scoped_session
 
 import os
 from dotenv import load_dotenv
@@ -33,21 +39,52 @@ class DBStorage():
 
     
     def all(self, cls=None):
-        pass
+        """ Queries on the current db session depending on cls name"""
+        result = {}
+        session = self.__session
+
+        if not cls:
+            tables = [User, Place, City, State, Amenity, Review]
+        else:
+            if type(cls) == str:
+                cls = eval(cls)
+                tables = [cls]
+
+        for table in tables:
+            instances = session.query(table).all()
+
+            for instance in instances:
+                key = f"{type(instance).__name__}.{instance.id}"
+                result[key] = instance            
+
+        return result
 
     def new(self, obj):
         """ Adds the object to the current db session"""
-        pass
+        if obj:
+            self.__session.add(obj)
 
     def save(self):
         """ Commit all xhanges of the current db session"""
-        pass
+        self.__session.commit()
 
     def delete(self, obj=None):
         """ Delete from hte current db session if obj not None"""
-        pass
+        if obj:
+            self.__session.delete(obj)
 
     def reload(self):
         """ Creates all tables in the db
-            Creates the current db session from the engine"""
+            Creates the current db session from the engine
+        """
+        Base.metadata.create_all(self.__engine)
+        session_factory = sessionmaker(bind=self.__engine,
+                                       expire_on_commit=False)
+        Session = scoped_session(session_factory)
+        self.__session = Session()
+
+    def close(self):
+        """
+        Closes Session"""
+        self.__session.close()
 
--- models/state.py
+++ models/state.py
@@ -4,16 +4,29 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from models.city import City
+import models
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class State(BaseModel, Base):
     """ The state class, contains state ID and name """
     __tablename__ = 'states'
 
     name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state',
+
+    if os.getenv('HBNB_TYPE_STORAGE') == 'DBStorage':
+        cities = relationship('City', backref='state',
                           cascade="add, delete, delete-orphan")
-    
-    @property
-    def cities(self):
-        return City
-    
+    else:
+        @property
+        def cities(self):
+            state_city = []
+            all_cities = models.storage.all(City)
+
+            for city in all_cities.values():
+                if city.state_id == self.id():
+                    state_city.append(city)
+
+            return state_city
